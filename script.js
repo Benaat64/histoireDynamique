17282336cadbbd9bc8203b2be9942f3da9b193dd
romain <romain.esquerra@oclock.school>
2024-05-21 11:40:40 +0200

    rajouter piste audio

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ function createImage(src) {
     img.src = src;
     return img;
 }
+function createMusique(song) {
+    const audio = document.createElement('audio');
+    audio.src = song;
+    return audio;
+}
 function createText(text) {
     const p = document.createElement('p');
     p.innerHTML = text;
@@ -39,13 +44,11 @@ function createButton(label, nextStoryId) {
 function handleSubmit(event) {
     event.preventDefault();
     const inputElement = document.querySelector('#prenom');
-    if (inputElement.value === '') {
-        alert('Veuillez entrer un prénom.');
-    } else {
+    if (inputElement.value !== '') {
         prenom = inputElement.value; // Stockez la valeur du prénom
-        localStorage.setItem('prenom', prenom); // Sauvegardez la valeur dans localStorage ! Blocage 1 weekend, car cette ligne est obligatoire !!!
-        choisirReponse(stories[currentStoryIndex].reponses[0].nextStoryId);
+        localStorage.setItem('prenom', prenom); // Sauvegardez la valeur dans localStorage
     }
+    choisirReponse(stories[currentStoryIndex].reponses[0].nextStoryId);
 }
 
 function displayStory() {
@@ -53,6 +56,11 @@ function displayStory() {
     const bodyElement = document.querySelector('body');
     bodyElement.innerHTML = ''; // Nettoyez le contenu précédent
     bodyElement.appendChild(createImage(story.img));
+    if (story.audio) {
+        const audioElement = createMusique(story.audio);
+        bodyElement.appendChild(audioElement);
+        audioElement.play();
+    }
     let storyText;
     if (typeof story.text === 'function') {
         storyText = story.text(prenom);
@@ -71,8 +79,12 @@ function displayStory() {
 
 function choisirReponse(nextStoryId) {
     const inputElement = document.querySelector('#prenom');
-    if (inputElement && inputElement.value !== '') {
-        prenom = inputElement.value; // Stockez la valeur du prénom
+    if (currentStoryIndex === 0) {
+        if (inputElement.value !== '') {
+            prenom = inputElement.value; // Stockez la valeur du prénom
+        }else{
+        return alert('Rajouter un prenom')
+        }
     }
     const nextStoryIndex = stories.findIndex(story => story.id === nextStoryId);
     if (nextStoryIndex !== -1) {
